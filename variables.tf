7fb239ad26569814b60610e2c941be379416d575
Anup Badharudheen <anupbdn@gmail.com>
2023-09-30 10:34:47 +0000

    #28 static website created , documented

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
+
+variable "index_html_filepath" {
+  description = "Path of index.html"
+  type = string  
+}
+
+variable "error_html_filepath" {
+  description = "Path of error.html"
+  type = string  
+}
